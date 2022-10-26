d57095dfc072485d6a59d47d112d8eade81fa910
samcms1234 <samdevpro99@gmail.com>
2022-10-26 14:54:52 +0530

    configured to Polygon Mumbai Testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const projectId = 
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    polygon_mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.API_KEY}`,
+      accounts: [
+        `0x${process.env.PRIVATE_KEY}`,
+      ],
+    },
+  },
 };

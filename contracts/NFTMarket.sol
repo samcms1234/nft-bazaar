64f5c5b535d76e2fe808fbcc437322666ad4b7be
samcms1234 <samdevpro99@gmail.com>
2022-10-26 18:44:10 +0530

    added function to fetch the unsold items

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -92,4 +92,22 @@ contract NFTMarket is ReentrancyGuard {
         _itemsSold.increment();
         payable(owner).transfer(listingPrice);
     }
+
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        uint itemCount = _itemIds.current();
+        uint unsoldItemCount = _itemIds.current() - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+        for(uint i = 0; i < itemCount; i++) {
+            if (idToMarketItem[i + 1].owner == address(0)) {
+                uint currentId = itToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
+    
 }
